8a39127698f4c22499ee424b899e605da097d201
Matheusfb <fialhobarbosamatheus@gmail.com>
2022-06-06 16:56:02 -0300

    Commit Inicial

---
+++ src/escalaglasgow/EscalaGlasgow.java
@@ -0,0 +1,13 @@
+package escalaglasgow;
+
+/**
+ *
+ * @author Matheus Fialho Barbosa
+ */
+public class EscalaGlasgow {
+    
+    public static void main(String[] args) {
+        new MenuPrincipal().setup();
+    }
+    
+}
